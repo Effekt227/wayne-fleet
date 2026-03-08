71bf422721438a1e6c313726eb4fd9fd021851d3
Effekt227 <milosnovy25@gmail.com>
2026-03-08 19:28:28 +0100

    Add SSL for Supabase pg8000 connection

--- database/database.py
+++ database/database.py
@@ -17,7 +17,7 @@ if _cloud_url:
     elif _cloud_url.startswith('postgresql://'):
         _cloud_url = _cloud_url.replace('postgresql://', 'postgresql+pg8000://', 1)
     DATABASE_URL = _cloud_url
-    engine = create_engine(DATABASE_URL, echo=False)
+    engine = create_engine(DATABASE_URL, echo=False, connect_args={"ssl_context": True})
     DB_PATH = '(PostgreSQL cloud)'
 else:
     DB_PATH = os.path.join(os.path.dirname(__file__), 'wayne_fleet.db')
